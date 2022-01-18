c5f95a3d934b033af7ac5a372544365773d956fc
Rodrigo Vieira <rvieira@id-brasil.matriz>
2022-01-18 06:40:29 -0300

    JavaScript

---
+++ Aula_JavaScript/Aula5 - Funções/assets/js/script.js
@@ -0,0 +1,13 @@
+// Funções
+function funcao(){
+    console.log("Funcao Funcionando");
+}
+
+funcao();
+
+// Funções com Parametros
+function paranmetro(a, b){
+    console.log(a+b);
+}
+
+paranmetro(10, 5);
