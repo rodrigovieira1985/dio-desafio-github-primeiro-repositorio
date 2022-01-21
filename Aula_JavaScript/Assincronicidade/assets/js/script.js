9d7067d657cb188050bf9353cf76776941f306b4
Rodrigo Vieira <rvieira@id-brasil.matriz>
2022-01-21 18:56:05 -0300

    JavaScript

--- Aula_JavaScript/Assincronicidade/assets/js/script.js
+++ Aula_JavaScript/Assincronicidade/assets/js/script.js
@@ -3,8 +3,45 @@
 
 
 //Promises
-const myPromise = new Promise((resolve, reject) => {
-    window.setTimeout(() => {
-        resolve(console.log('Resolvida!'));
-    }, 2000);
-});
+// const myPromise1 = new Promise((resolve, reject) => {
+//     window.setTimeout(() => {
+//         resolve(console.log('Resolvida 1!'));
+//     }, 2000);
+// });
+
+
+
+// Async e Await
+// async function resolvePromise1() {
+//     const myPromise2 = new Promise ((resolve2, reject) => {
+//         window.setTimeout(() => {
+//             resolve2('Resolvida 2!');
+//         }, 3000);
+//     });
+//     const resolved1 = await myPromise2
+//             .then((result) => result + 'Passando pelo Then')
+//             .then((result) => result + ' e Agora Acabou!! ')
+//             .catch((err) => console.log(err.message));
+//     return resolved1;        
+// }
+
+
+// Async e Await com try Catch
+async function resolvePromise2() {
+    const myPromise2 = new Promise((resolve2, reject) => {
+        window.setTimeout(() => {
+            resolve2('Resolvida 2!');
+        }, 3000);
+    });
+    let results;
+    try{
+    results = await myPromise2
+            .then((result) => result + 'Passando pelo Then')
+            .then((result) => result + ' e Agora Acabou!! ')
+            .catch((err) => console.log(err.message));
+    }catch(err){
+            results = err.message; 
+    }
+    return results;
+
+}
